5fc08258fc07787a43dcedd982b53915ed50662c
Ryan <ryan314r@gmail.com>
2018-01-17 21:17:10 -0600

    Switch to the Slackware vm.

--- finish.ps1
+++
@@ -1,7 +0,0 @@
-# Remove the releases
-Write-Host "Removing releases"
-plink ryan@zoom.pi 'rm -f ~/cs327-releases/*'
-
-# Move the build to the desktop
-Write-Host "Moving tar ball to desktop"
-move *.tar.gz c:\users\ryan3\Desktop
